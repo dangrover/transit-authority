ddf80d00d9a622506d89452550cc39ffd847f162
Dan Grover <dan@dangrover.com>
2014-06-08 19:02:10 -0700

    adjusted appearance of placeholder nodes

--- Transit Authority/POIPlaceholderNode.m
+++ Transit Authority/POIPlaceholderNode.m
@@ -26,19 +26,19 @@
         _dottedLine = [CCSprite spriteWithImageNamed:@"poi-placeholder.png"];
         _nameLabel = [[CCLabelTTF alloc] initWithString:displayName
                                                fontName:@"Raleway"
-                                               fontSize:22];
+                                               fontSize:18];
         
         
         [self addChild:_dottedLine];
         [self addChild:_glyph];
         [self addChild:_nameLabel];
         
-        
         _nameLabel.position = CGPointMake(0, -60);
         _nameLabel.color = [CCColor colorWithCcColor3b:ccc3(125, 125, 125)];
         _glyph.scale = 1.25;
+        _glyph.opacity = 0.5;
         _dottedLine.scale = 0.6;
-        _dottedLine.opacity = 200;
+        _dottedLine.opacity = 0.25;
     }
     
     return self;
